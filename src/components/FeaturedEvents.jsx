a9f3539593f9482cfd883adeb5629e09c0601c86
Sri-Nidhi-25 <140502551+Sri-Nidhi-25@users.noreply.github.com>
2024-10-05 12:05:04 +0530

    resizing event container

--- src/components/FeaturedEvents.jsx
+++ src/components/FeaturedEvents.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const FeaturedEvents = () => {
   return (
-    <div className="relative w-[994px] h-[340px] expandevents">
-      <div className="absolute inset-0 rounded-[10px] shadow-lg border border-[#cecece] backdrop-blur-[20px] p-4">
+    <div className="relative w-[60rem] h-[15rem] mt-3 expandevents">
+      <div className="absolute inset-0 rounded-[10px] shadow-lg border border-[#cecece] backdrop-blur-[20px] p-3">
         {/* Top section: Event Image & Title */}
         <div className="flex">
           <img 
-            className="w-[288px] h-[295px] object-cover rounded-md" 
+            className="w-[250px] h-[200px] object-cover rounded-md" 
             src="https://via.placeholder.com/288x295" 
             alt="Event"
           />
@@ -16,14 +16,14 @@ const FeaturedEvents = () => {
               🌟 Join us for an "Exclusive Workshop," hosted by Code Crafters, IITM BS Degree Coding Club! 🌟
             </h2>
             {/*<div className="mt-2 text-sm text-[#a2a2a2] font-medium">50 Reviews</div>*/}
-            <div className="flex mt-4 text-white font-montserrat text-[15px]">
-              <div className="font-bold text-[17px] space-y-3">
+            <div className="flex mt-2 text-white font-montserrat text-[13px]">
+              <div className="font-bold text-[13px] space-y-2">
                 <p>Date:</p>
                 <p>Time:</p>
                 <p>Location:</p>
                 <p>Guest Speaker:</p>
               </div>
-              <div className="ml-6 space-y-3">
+              <div className="ml-6 space-y-2">
                 <p>9th June, 2024</p>
                 <p>Sunday 9:00 PM IST</p>
                 <p>Zoom</p>
@@ -31,11 +31,11 @@ const FeaturedEvents = () => {
               </div>
             </div>
             {/* Registered Users Section */}
-            <div className="flex items-center mt-6">
-              <div className="bg-violet-700 rounded-full w-16 h-[26px] flex justify-center items-center">
-                <span className="text-white text-xl font-semibold">151+</span>
+            <div className="flex items-center mt-2">
+              <div className="bg-violet-700 rounded-full w-12 h-[15px] flex justify-center items-center">
+                <span className="text-white text-m font-semibold">151+</span>
               </div>
-              <div className="ml-4 text-white/80 text-xl font-bold">Already Registered</div>
+              <div className="ml-4 text-white/80 text-m font-bold">Already Registered</div>
             </div>
           </div>
         </div>
